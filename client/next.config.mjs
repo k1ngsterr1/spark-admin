92937aa3728b6afbdda45ca3e736045e0a0fe972
senseiGai <swerss564@gmail.com>
2024-02-15 00:42:13 +0600

    version 1.0.3

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,4 +1,16 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+function NextConfig(config) {
+  return config;
+}
 
-export default nextConfig;
+export default NextConfig({
+  reactStrictmode: true,
+  swcMinify: true,
+
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: [{ loader: "@svgr/webpack", options: { icon: true } }],
+    });
+    return config;
+  },
+});
