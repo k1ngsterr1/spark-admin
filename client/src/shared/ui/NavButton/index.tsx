a98e43972ebaf337fb16db27271f758c0c3abe18
Ruslan Makhmatov <ruslanmakhmatov@gmail.com>
2024-02-16 18:44:29 +0600

    Version 1.0.5

--- client/src/shared/ui/NavButton/index.tsx
+++ client/src/shared/ui/NavButton/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 import styles from "./styles.module.scss";
+
 import { IconDefinition } from "@fortawesome/fontawesome-svg-core";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
@@ -14,10 +15,9 @@ export const NavButton: React.FC<NavButtonProps> = ({
   margin,
   icon,
   text,
-  ...rest // Now rest includes all optional button HTML attributes
+  ...rest
 }) => {
   return (
-    // Apply to pass all additional standard button attributes
     <button className={`${styles.nav_button} ${margin}`} {...rest}>
       <FontAwesomeIcon icon={icon} className={styles.nav_button__icon} />
       <span className={styles.nav_button__text}>{text}</span>
