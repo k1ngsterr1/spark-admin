e9b483a4fdee92a0c10ac70c07772a1aa9eaf7c9
Ruslan Makhmatov <ruslanmakhmatov@gmail.com>
2024-02-16 21:20:48 +0600

    Version 1.0.8

--- client/src/features/Header/index.tsx
+++ client/src/features/Header/index.tsx
@@ -2,11 +2,13 @@ import React from "react";
 
 import styles from "./styles.module.scss";
 import { UserTab } from "@entities/UserTab";
+import { SearchBar } from "@features/SearchBar";
 
 export const Header = () => {
   return (
     <header className={styles.header}>
       <div className={styles.header__content}>
+        <SearchBar />
         <UserTab name="Text" photo="aaa" />
       </div>
     </header>
