28971e3a00ac5ae2f38eb95b3092359b03885432
senseiGai <swerss564@gmail.com>
2024-02-21 12:49:29 +0600

    version 1.1.4

--- client/src/widgets/Form/EmailConfirmationForm/index.tsx
+++ client/src/widgets/Form/EmailConfirmationForm/index.tsx
@@ -1,8 +1,10 @@
 import { Button } from "@shared/ui/Buttons";
 import Input from "@shared/ui/Inputs/DefaultInport";
 import Heading from "@shared/ui/Heading/index";
+import MiniText from "@shared/ui/MiniText";
 
 import styles from "../styles/styles.module.scss";
+// import "../EmailConfirmationForm/styles.scss";
 import "../../../shared/styles/mixins.scss";
 import SparkLogo from "@assets/spark_product_logo.svg";
 
@@ -14,18 +16,27 @@ const EmailConfirm = () => {
           <SparkLogo />
         </div>
         <Heading text="Подтверждение почты" margin="mt-8" />
-        <div className="text_orange">
+        <div className="text-user">
           <span className="text_with_detail">
-            Аккаунт:
-            <span className="text_orange">smth</span>
+            Аккаунт: 
+            <span className="text_orange"> smth</span>
           </span>
         </div>
-        <div className={styles.registration__mini_text_account_mail}>
-          <span>Письмо с подтверждением отправлена на почту </span>
-          <div className={styles.login__orange}>ruslanmakmhatom@gmail.com</div>
+        <div className="text-mail">
+          <span className="text_with_detail">Письмо с подтверждением отправлена на почту 
+              <div><span className="text_orange">ruslanmakmhatom@gmail.com</span></div>
+          </span>
         </div>
         <form className={styles.registration__form}>
-          <Button text="Подтвердить" buttonType="regular" margin="mt-8" />
+          <div className={styles.confirmation_input}>
+          <Input inputType="email" maxLength={1} autoComplete="none"/>
+          <Input inputType="email" maxLength={1} autoComplete="none"/>
+          <Input inputType="email" maxLength={1} autoComplete="none"/>
+          <Input inputType="email" maxLength={1} autoComplete="none"/>
+          <Input inputType="email" maxLength={1} autoComplete="none"/>
+          </div>
+          <Button text="Подтвердить" buttonType="regular" margin="mt-16" />
+          <MiniText linktext="Отправить еще раз" text="Не пришел код?" href="email-confirmation" margin="mt-4"/>
         </form>
       </div>
     </section>
