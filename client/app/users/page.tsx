4da0a3259c4c3610481639dd6aef5d16f3ce76fc
fwefew123 <artem.andreev200012@mail.ru>
2024-02-22 22:36:50 +0600

    Version 1.1.6

--- client/app/users/page.tsx
+++ client/app/users/page.tsx
@@ -1,9 +1,13 @@
+import { Menu } from "@features/Menu/index";
 import { Users } from "@pages/usersPage/index";
 import React from "react";
 
+import styles from "./styles.module.scss";
+
 export const UsersPage = () => {
   return (
-    <div>
+    <div className={styles.user}>
+      <Menu />
       <Users />
     </div>
   );
--- client/src/shared/ui/Selector/index.tsx
+++ client/src/shared/ui/Selector/index.tsx
@@ -1,29 +1,36 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import styles from './style.module.scss'
+import styles from "./style.module.scss";
 
 interface SelectorProps {
-    items: string[];
-    className?: string;
-    onChange: (selectedOption: string) => void;
+  items: string[];
+  className?: string;
+  onChange: (selectedOption: string) => void;
 }
 
-export const Selector: React.FC<SelectorProps> = ({ items, onChange, className }) => {
-    const [isOpen, setIsOpen] = useState(false);
+export const Selector: React.FC<SelectorProps> = ({
+  items,
+  onChange,
+  className,
+}) => {
+  const [isOpen, setIsOpen] = useState(false);
 
+  const handleOptionClick = (selectedOption: string) => {
+    onChange(selectedOption);
+    setIsOpen(false);
+  };
 
-    const handleOptionClick = (selectedOption: string) => {
-        onChange(selectedOption);
-        setIsOpen(false);
-    };
-
-    return (
-        <div className={`${styles.selector} ${className || ''}`.trim()}>
-            {items.map((item, index) => (
-                <div className={styles.selector__items} key={index} onClick={() => handleOptionClick(item)}>
-                    {item}
-                </div>
-            ))}
+  return (
+    <div className={`${styles.selector} ${className || ""}`.trim()}>
+      {items.map((item, index) => (
+        <div
+          className={styles.selector__items}
+          key={index}
+          onClick={() => handleOptionClick(item)}
+        >
+          {item}
         </div>
-    );
+      ))}
+    </div>
+  );
 };
