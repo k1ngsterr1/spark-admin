005f35521f6aeb1d31a457e3c898cc7b5b521db5
Ruslan Makhmatov <ruslanmakhmatov@gmail.com>
2024-02-23 17:18:41 +0600

    Version 1.3.1 --ADD_USERS_UNTESTED

--- server/controllers/websiteController.ts
+++ server/controllers/websiteController.ts
@@ -1,8 +1,6 @@
-import { Website } from "@models/websiteModel";
 import { User } from "@models/userModel";
+import { Website } from "@models/websiteModel";
 import sequelize from "config/sequelize";
-
-const { Op } = require("sequelize");
 
 class WebsiteController {
   async addWebsite(req, res) {
@@ -41,11 +39,65 @@ class WebsiteController {
       const websites = await websiteRepositry.findAll({
         where: { owner: userID },
       });
+
+      const optimizedWebsites = websites.map((website) => {
+        return {
+          name: website.name,
+          url: website.url,
+          owner: website.owner,
+          users: website.users,
+          id: website.id,
+        };
+      });
+
       console.log(userID);
-      return res.status(201).json(websites);
+      return res.status(201).json(optimizedWebsites);
     } catch (error) {
       console.error("Error with fetching websites:", error);
       return res.status(500).json({ error: "Failed to get websites" });
+    }
+  }
+
+  async addUser(req, res) {
+    try {
+      const { userEmail, userRole, websiteId } = req.body;
+      const requesterID = req.user.id;
+
+      const userRepository = sequelize.getRepository(User);
+      const websiteRepository = sequelize.getRepository(Website);
+      const website = await websiteRepository.findByPk(websiteId);
+
+      if (!website) {
+        return res.status(404).json({ message: "Website not found" });
+      }
+
+      const isOwner = website.owner === requesterID;
+
+      if (!isOwner) {
+        return res
+          .status(403)
+          .json({ message: "You are not the owner of this website" });
+      }
+      const userToAdd = await userRepository.findOne({
+        where: { email: userEmail },
+      });
+
+      if (!userToAdd) {
+        return res.status(404).json({ message: "User not found" });
+      }
+
+      const newUserItem = {
+        id: userToAdd.id,
+        email: userEmail,
+        role: userRole,
+      };
+
+      website.users = [...website.users, newUserItem];
+      await website.save();
+      res.status(200).json({ message: "User added successfully", website });
+    } catch (error) {
+      console.error("Error adding user:", error);
+      res.status(500).json({ error: "Failed to add user" });
     }
   }
 }
