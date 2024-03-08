9fd249b29c01ea0cb0d8495b947eb22e0f7e6ea0
k1ngsterr1 <ruslanmakhmatov@gmail.com>
2024-03-08 22:10:07 +0600

    Version 1.8.2

--- client/src/entities/Popup_Components/CodePopup/index.tsx
+++ client/src/entities/Popup_Components/CodePopup/index.tsx
@@ -1,17 +1,28 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import Logo from "@assets/spark_product_logo.svg";
 import styles from "./styles.module.scss";
+import PopupGeneric from "@shared/ui/Generic_Components/PopupGeneric";
+import { useUserPopup } from "@shared/lib/contexts/AppContext";
 
 export const CodePopup = () => {
+  const { isWebPopupVisible, toggleWebPopup } = useUserPopup();
+
+  if (!isWebPopupVisible) {
+    return null;
+  }
+
   return (
-    <div className={styles.overlay}>
-      <div className={styles.website_popup}>
+    <PopupGeneric onClose={toggleWebPopup}>
+      <div
+        onClick={(e) => e.stopPropagation()}
+        className={styles.website_popup}
+      >
         <div className={styles.website_popup__logo}>
           <Logo />
         </div>
         <span className={styles.website_popup__text}>Добавьте ваш сайт</span>
       </div>
-    </div>
+    </PopupGeneric>
   );
 };
