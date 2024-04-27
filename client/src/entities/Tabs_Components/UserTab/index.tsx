896012f311783549a051f7a89641ce53c41f363f
k1ngsterr1 <=>
2024-04-28 01:31:37 +0500

    --AVATAR ADDED

--- client/src/entities/Tabs_Components/UserTab/index.tsx
+++ client/src/entities/Tabs_Components/UserTab/index.tsx
@@ -1,4 +1,6 @@
+'use client'
 import React from "react";
+import { useTheme } from 'next-themes'
 
 import styles from "./styles.module.scss";
 
@@ -8,11 +10,13 @@ interface UserTabProps {
 }
 
 export const UserTab: React.FC<UserTabProps> = ({ name, photo }) => {
+  const { theme } = useTheme()
+
   return (
     <div className={styles.user_tab}>
       <span className={styles.user_tab__name}>{name}</span>
       <img
-        src={photo}
+        src={ theme === 'light' ? 'https://i.ibb.co.com/wS1Q77R/avatar.png' : 'https://i.ibb.co.com/KjvYd53/avatar-black.png'}
         alt={`${name}'s profile photo`}
         className={styles.user_tab__photo}
       />
