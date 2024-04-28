868b3967dd2fe5d288e0ced79955cc0b48987aa6
Ruslan Makhmatov <103198805+k1ngsterr1@users.noreply.github.com>
2024-04-28 23:11:55 +0600

    --CHANGE PASSWORD IS WORKING

--- client/src/shared/lib/hooks/Form/useChangePassword.ts
+++ client/src/shared/lib/hooks/Form/useChangePassword.ts
@@ -7,30 +7,34 @@ interface IData {
   code: string;
 }
 
-export async function useChangePassword(data: IData): Promise<string | void> {
-  try {
-    const userData = useUserData();
-    const accessToken = userData.accessToken;
+export const useChangePassword = () => {
+  const userData = useUserData();
+  const accessToken = userData.accessToken;
 
-    const response = await axios.post(
-      "https://spark-admin-production.up.railway.app/api/auth/change-password",
-      data,
-      {
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${accessToken}`,
-        },
+  const changePassword = async (data: IData) => {
+    try {
+      const response = await axios.post(
+        "https://spark-admin-production.up.railway.app/api/auth/change-password",
+        data,
+        {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${userData.accessToken}`,
+          },
+        }
+      );
+      console.log("Data created:", response.data);
+
+      window.location.href = "/login";
+    } catch (error: any) {
+      console.error("Failed to create data:", error);
+      if (error.response) {
+        return error.response.data.message;
+      } else {
+        return "An unexpected error occurred";
       }
-    );
-    console.log("Data created:", response.data);
-
-    window.location.href = "/login";
-  } catch (error: any) {
-    console.error("Failed to create data:", error);
-    if (error.response) {
-      return error.response.data.message;
-    } else {
-      return "An unexpected error occurred";
     }
-  }
-}
+  };
+
+  return changePassword;
+};
--- client/src/shared/lib/hooks/Form/useSubmitChangePassword.ts
+++ client/src/shared/lib/hooks/Form/useSubmitChangePassword.ts
@@ -8,16 +8,14 @@ export const useSubmitChangePassword = () => {
   const [newPassword, setNewPassword] = useState("");
   const [passwordError, setPasswordError] = useState("");
   const [code, setCode] = useState("");
+  const changePassword = useChangePassword();
 
   const userData = useUserData();
 
   const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
 
-    const result = await useChangePassword({ code, newPassword });
-    if (typeof result === "string") {
-      setPasswordError(result);
-    }
+    await changePassword({ newPassword, code });
   };
 
   return {
