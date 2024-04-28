0726b7e7dd293c296d8b30996029d2fcc6be18f9
Ruslan Makhmatov <103198805+k1ngsterr1@users.noreply.github.com>
2024-04-28 18:37:32 +0600

    --ADDED USERDATA

--- client/src/shared/lib/hooks/Form/useValidate.ts
+++ client/src/shared/lib/hooks/Form/useValidate.ts
@@ -1,22 +1,22 @@
-'use client'
+"use client";
 
 import { useState } from "react";
 
-
-
 export const useFieldValidator = () => {
-    const [error, setError] = useState<string>('');
-  
-    const validateField = (value:string, regex:RegExp, errorMessage:string) => {
-      if (!regex.test(value)) {
-        setError(errorMessage);
-        return false;
-      }
-      setError('');
-      return true;
-    };
-  
-    return { error, validateField };
-  };
-  
+  const [error, setError] = useState<string>("");
 
+  const validateField = (
+    value: string,
+    regex: RegExp,
+    errorMessage: string
+  ) => {
+    if (!regex.test(value)) {
+      setError(errorMessage);
+      return false;
+    }
+    setError("");
+    return true;
+  };
+
+  return { error, validateField };
+};
