ce7d13b5befba2b5f467170e88b435d8c1decb47
Ruslan Makhmatov <103198805+k1ngsterr1@users.noreply.github.com>
2024-04-28 14:32:57 +0600

    --CHECK WEBSITE FIXED

--- server/dist/core/use_cases/Website/CheckWebsite.ts
+++ server/dist/core/use_cases/Website/CheckWebsite.ts
@@ -1,11 +1,25 @@
+import { WebsiteRepository } from "@infrastructure/repositories/WebsiteRepository";
 import WebsiteService from "@services/websiteService";
 
 // Подгрузка сайта и дальнейшая проверка веб-сайта на наличие нашего тэга
 
 export class CheckWebsite {
-  constructor(private websiteService: WebsiteService) {}
-  async execute(url: string, expectedCode: string): Promise<boolean> {
-    const html = await this.websiteService.fetchHTMLContent(url);
-    return this.websiteService.checkMetaTag(html, expectedCode);
+  constructor(
+    private websiteService: WebsiteService,
+    private WebsiteRepository
+  ) {}
+  async execute(
+    url: string,
+    expectedCode: string
+  ): Promise<{ exists: boolean; isValid: boolean }> {
+    const website = await this.WebsiteRepository.findByUrl(url);
+
+    if (!website) {
+      return { exists: false, isValid: false };
+    }
+
+    const html = await this.websiteService.fetchHTMLContent(website.url);
+    const isValid = await this.websiteService.checkMetaTag(html, expectedCode);
+    return { exists: true, isValid };
   }
 }
