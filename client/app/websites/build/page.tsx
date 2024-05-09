b297be2ac10b70714f8afd3a7e73a557a84c9779
k1ngsterr1 <=>
2024-05-09 13:48:39 +0500

    --ADDED PAGE SELECTORS

--- client/app/websites/build/page.tsx
+++ client/app/websites/build/page.tsx
@@ -1,17 +1,21 @@
+import React from "react";
+import Heading from "@shared/ui/Heading";
+
+import { PageCard } from "@entities/PageCard";
 import { Header } from "@features/Header";
 import { Menu } from "@features/Menu";
 import { PageTypeSelector } from "@features/PageTypeSelector";
-import Heading from "@shared/ui/Heading";
-import React from "react";
+import { PageCardsLayout } from "@features/PageCardsLayout";
 
 const BuildWebsite = () => {
   return (
     <div className="flex">
       <Menu />
-      <main className="flex flex-col w-full">
+      <main className="flex flex-col w-full ml-16">
         <Header />
         <Heading text="Новая страница" />
         <PageTypeSelector />
+        <PageCardsLayout pageType="a" />
       </main>
     </div>
   );
