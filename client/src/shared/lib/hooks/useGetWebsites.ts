b7d4a33265674d626175a01422e3c49407191999
k1ngsterr1 <=>
2024-05-11 20:39:54 +0500

    --TRYING TO FIX MY GITHUB

--- client/src/shared/lib/hooks/useGetWebsites.ts
+++ client/src/shared/lib/hooks/useGetWebsites.ts
@@ -1,7 +1,7 @@
 // функция для получения списка вебсайтов
 export async function useGetWebsites() {
   const token =
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MiwiZW1haWwiOiJydXNsYW5tYWtobWF0b3ZAZ21haWwuY29tIiwicm9sZSI6InVzZXIiLCJpYXQiOjE3MTUxNzE1OTUsImV4cCI6MTcxNTI1Nzk5NX0.Rf-CKw_5ozzdDUqSBll9Gt9H-AUTQ2zJoEHF_xA0OhY";
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MiwiZW1haWwiOiJydXNsYW5tYWtobWF0b3ZAZ21haWwuY29tIiwicm9sZSI6InVzZXIiLCJpYXQiOjE3MTUyNTgzOTUsImV4cCI6MTcxNTM0NDc5NX0.xdm-dpZfFUThMeHLHx0seJQkzjtp4_GywiAwJDGl_n0";
   const url = "https://spark-admin-production.up.railway.app/api/website";
 
   const response = await fetch(url, {
