917c919a738ad931fec9c03f79b6792a2c9177ac
k1ngsterr1 <=>
2024-05-16 00:28:48 +0500

    --CONTENT IS FETCHING

--- client/app/websites/build/[slug]/page.tsx
+++ client/app/websites/build/[slug]/page.tsx
@@ -1,16 +1,13 @@
 "use client";
-import axios from "axios";
+import { useFetchPageContent } from "@shared/lib/hooks/useFetchPageContent";
 import { useParams } from "next/navigation";
 
 //  Тут будет редактируемая страница
-export default function DynamicPage() {
+export default function ChangablePageContent() {
   const { slug } = useParams();
-  const url = `http://spark-admin-production.up.railway.app/api/page-card/render/${slug}`;
+  const {htmlContent} = useFetchPageContent(slug)
 
-  const response = axios.get(url);
-  const htmlContent = response.data;
 
-  console.log("HTML Content:", htmlContent); // This will log the HTML content in your console
 
   return <div dangerouslySetInnerHTML={{ __html: htmlContent }} />;
 }
