35322a23700512d5bdb0d1d92dae1030d1ee14a6
Erlan-Bam <erlanzh.gg@gmail.com>
2024-05-15 14:51:03 +0500

    --SWAGGER FOR BUSINESS LANDING

--- server/dist/infrastructure/routes/pageCardRoutes.ts
+++ server/dist/infrastructure/routes/pageCardRoutes.ts
@@ -12,12 +12,12 @@ const router = express.Router();
 /**
  * @swagger
  * /api/page-card/add:
- * components:
- *   securitySchemes:
- *     bearerAuth:
- *       type: http
- *       scheme: bearer
- * post:
+ *   components:
+ *     securitySchemes:
+ *       bearerAuth:
+ *         type: http
+ *         scheme: bearer
+ *   post:
  *     summary: Добавление карточки страницы
  *     description: Создает новую карточку страницы с данными, предоставленными пользователем.
  *     tags:
@@ -57,8 +57,57 @@ const router = express.Router();
  *         description: Произошла серверная ошибка.
  */
 router.post("/add", (req, res) => pageCardController.addPageCard(req, res));
+
+/**
+ * @swagger
+ * /api/page-card/add-block:
+ *   post:
+ *     summary: Добавление блока в карточку страницы
+ *     description: Добавляет новый блок в карточку страницы, используя имя блока и имя карточки страницы
+ *     tags:
+ *       - Карточки страниц
+ *     security:
+ *       - bearerAuth: []
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - blockName
+ *               - pageCardName
+ *             properties:
+ *               blockName:
+ *                 type: string
+ *                 description: Название блока, который будет добавлен
+ *               pageCardName:
+ *                 type: string
+ *                 description: Название карточки страницы, в которую добавляется блок
+ *     responses:
+ *       200:
+ *         description: Блок успешно добавлен в карточку страницы.
+ *       400:
+ *         description: Ошибка в данных.
+ *       500:
+ *         description: Произошла серверная ошибка.
+ */
 router.post("/add-block", (req, res) => pageCardController.addBlock(req, res));
 
+/**
+ * @swagger
+ * /api/page-card/render/business-landing:
+ *   get:
+ *     summary: Отображение бизнес-лендинга
+ *     description: Создает бизнес лендинг из разных данних блоков
+ *     tags:
+ *       - Шаблоны
+ *     responses:
+ *       200:
+ *         description: Страница успешно отображена.
+ *       500:
+ *         description: Произошла серверная ошибка.
+ */
 router.get("/render/business-landing", (req, res) => pageCardController.businessLanding(req, res));
 
 export default router;
