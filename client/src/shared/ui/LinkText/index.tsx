79a08e4cf0dea624c245f0c358711b277361e94f
senseiGai <swerss564@gmail.com>
2024-05-22 17:14:05 +0500

    BUILD HEADER

---
+++ client/src/shared/ui/LinkText/index.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Link } from 'react-scroll';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+import styles from './styles.module.scss';
+import { IconDefinition } from '@fortawesome/free-solid-svg-icons';
+
+interface LinkProps {
+  text: string;
+  to: string;
+  href: string;
+  linkTextType: 'header' | 'footer';
+  onClick?: () => void;
+  icon?: IconDefinition | undefined;
+}
+
+const LinkText: React.FC<LinkProps> = ({ text, to, href, linkTextType, icon, onClick }) => {
+  const handleClick = (event: React.MouseEvent) => {
+    event.preventDefault();
+    if (window.location.pathname !== '/') {
+      window.location.href = '/';
+    } else if (onClick) {
+      onClick();
+    }
+  };
+
+  const linkTextClass = `${styles.linktext} ${styles[`link--${linkTextType}`]}`;
+
+  return (
+    <Link className={linkTextClass} to={to} href={href} smooth onClick={handleClick}>
+      {icon && <FontAwesomeIcon className={styles.icon} icon={icon}/>}
+      {text}
+    </Link>
+  );
+};
+
+export default LinkText;
