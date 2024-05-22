837945a2f78415326e3e8a15b310af50504722a5
Ruslan Makhmatov <103198805+k1ngsterr1@users.noreply.github.com>
2024-05-22 23:42:11 +0600

    --ADDED BLOCK BUTTONS

--- client/app/websites/build/[slug]/page.tsx
+++ client/app/websites/build/[slug]/page.tsx
@@ -1,28 +1,50 @@
 "use client";
+import React from "react";
 import { useFetchPageContent } from "@shared/lib/hooks/useFetchPageContent";
 import { useParseHTMLContent } from "@shared/lib/hooks/useParseHTMLContent";
 import { useParams } from "next/navigation";
-import React from "react";
+import { BlockButton } from "@shared/ui/BlockButton";
+import {
+  faDownLong,
+  faTrash,
+  faUpLong,
+} from "@fortawesome/free-solid-svg-icons";
+import { HeaderEditor } from "@features/HeaderEditor";
+import { AddBlockButton } from "@shared/ui/AddBlockButton";
+
+import styles from "./styles.module.scss";
 
 //  Тут будет редактируемая страница
 export default function ChangablePageContent() {
   const { slug } = useParams();
   const { htmlContent } = useFetchPageContent(slug);
-  const cleanContent = htmlContent?.replace(/<\/?body>|<\/?html>/gi, ""); // Remove body and html tags
-  const { content } = useParseHTMLContent(cleanContent);
 
-  const parsedContent = content.map((item, index) => (
-    <React.Fragment key={index}>{item}</React.Fragment>
-  ));
+  // Очищаем контент от лишних Body и HTML Тэгов
+  const cleanContent = htmlContent?.replace(/<\/?body>|<\/?html>/gi, "");
+  const { sections } = useParseHTMLContent(cleanContent);
 
-  console.log(content);
-
-  // return <div dangerouslySetInnerHTML={{ __html: htmlContent }} id="zhopa" />;
   return (
-    <div>
-      {content.map((item, index) => (
-        <React.Fragment key={index}>{item}</React.Fragment>
+    <>
+      <HeaderEditor
+        isLoading={false}
+        websiteName="Test"
+        websiteURL="Test"
+        pageURL="Test"
+        pageType="Test"
+      />
+      {sections.map((item, index) => (
+        <div key={index} className={styles.content_container}>
+          <div className="w-[95%] flex justify-end gap-2 m-auto pt-3 pb-3">
+            <BlockButton icon={faUpLong} onClick={() => console.log("LOL")} />
+            <BlockButton icon={faDownLong} onClick={() => console.log("LOL")} />
+            <BlockButton icon={faTrash} onClick={() => console.log("LOL")} />
+          </div>
+          {item}
+          <div className="w-full flex justify-center items-center">
+            <AddBlockButton />
+          </div>
+        </div>
       ))}
-    </div>
+    </>
   );
 }
--- client/src/shared/lib/hooks/useParseHTMLContent.tsx
+++ client/src/shared/lib/hooks/useParseHTMLContent.tsx
@@ -2,34 +2,16 @@ import React, { useState, useEffect } from "react";
 import parse from "html-react-parser";
 
 export const useParseHTMLContent = (htmlContent: string) => {
-  const [content, setContent] = useState<React.ReactNode[]>([]);
-
-  const handleAddBlock = (index: number) => {
-    console.log("Add block at index:", index);
-  };
+  const [sections, setSections] = useState<React.ReactNode[]>([]);
 
   useEffect(() => {
     if (htmlContent) {
-      const sections = htmlContent.split(/<\/section>/);
-      const contentWithButtons = sections.flatMap((section, index) => [
-        parse(`${section}</section>`),
-        <button
-          key={`btn-${index}`}
-          className="bg-red-500 w-20 h-20 rounded-full"
-          onClick={() => handleAddBlock(index)}
-        >
-          +
-        </button>,
-      ]);
-      setContent(contentWithButtons);
-
-      console.log(
-        content.map((item) =>
-          React.isValidElement(item) ? "Valid React Element" : "Invalid Element"
-        )
-      );
+      const parsedSections = htmlContent
+        .split(/<\/section>/)
+        .map((section, index) => parse(`${section}</section>`));
+      setSections(parsedSections);
     }
   }, [htmlContent]);
 
-  return { content };
+  return { sections };
 };
