14e18706a6792489491b56327299b14cc7877a06
TomBebrov1ch <artem.andreev200012@mail.ru>
2024-05-24 16:00:17 +0500

    --FRONTEND UPDATED

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -2,9 +2,10 @@ import React from "react";
 import { MainPageHeader } from "@features/MainPageHeader";
 import { TypeHeading } from "@shared/ui/TypeHeading";
 import Cursor from "@shared/ui/Cursor";
+import DynamicGrid from "@shared/ui/DynamicGrid";
 
 import styles from "./styles.module.scss";
-import DynamicGrid from "@shared/ui/DynamicGrid";
+import { ButtonLink } from "@shared/ui/Buttons_Components/Buttons";
 
 const MainPage = () => {
   return (
@@ -23,6 +24,16 @@ const MainPage = () => {
             repeat={0}
             text={"Spark Admin"}
           />
+          <p className={styles.container__paragraph}>
+            Наши инновационные подходы позволяют создавать уникальные и
+            эффективные веб-сайты, адаптированные под ваши нужды. Мы используем
+            передовые технологии, обеспечивая быстрый, безопасный и удобный
+            пользовательский опыт. С нашими решениями ваш веб-сайт будет
+            выделяться среди конкурентов и привлекать больше посетителей.
+          </p>
+          <div className="flex items-center justify-center gap-4 mt-12">
+            <ButtonLink text="Войти" buttonType="regular" href="login" />
+          </div>
         </div>
       </div>
     </>
