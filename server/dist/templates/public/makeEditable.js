109d2563f7f86e054e4c8d846832007ba99ebd83
Erlan-Bam <erlanzh.gg@gmail.com>
2024-05-27 16:23:00 +0500

    --REMOVED TRIM

--- server/dist/templates/public/makeEditable.js
+++ server/dist/templates/public/makeEditable.js
@@ -1,8 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
   function makeElementsEditable() {
-    const editableElements = document.querySelectorAll(
-      "[id^='editable-agro-']"
-    );
+    const editableElements = document.querySelectorAll("[id^='editable-agro-']");
 
     editableElements.forEach((element) => {
       if (!element.getAttribute("contenteditable")) {
@@ -12,16 +10,12 @@ document.addEventListener("DOMContentLoaded", function () {
       if (!element.getAttribute("data-blur-listener-added")) {
         element.setAttribute("data-blur-listener-added", "true");
         element.addEventListener("blur", function () {
-          const info = element.id.split("-");
-          const siteName = info[1];
-          const componentId = info[2];
+          const [_, siteName, componentId] = element.id.split("-");
           const route = `https://spark-admin-production.up.railway.app/api/site/update/${siteName}/${componentId}`;
 
           const data = {
-            newValue: element.innerHTML,
+            newValue: element.textContent.replace(/\u00a0/g, ' ').trim()
           };
-          data.newValue = data.newValue.replace(/\n\s*/g, '');
-          data.newValue = data.newValue.trim();
 
           fetch(route, {
             method: "POST",
@@ -42,10 +36,8 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   }
 
-  // Initial check for elements
   makeElementsEditable();
 
-  // Set up a MutationObserver to watch for changes in the DOM
   const observer = new MutationObserver((mutations) => {
     mutations.forEach((mutation) => {
       if (mutation.type === "childList" || mutation.type === "attributes") {
@@ -54,10 +46,9 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   });
 
-  // Start observing the document body for changes
   observer.observe(document.body, {
-    childList: true, // Watch for added or removed child nodes
-    attributes: true, // Watch for changes to attributes
-    subtree: true, // Watch the entire subtree for changes
+    childList: true,
+    attributes: true,
+    subtree: true,
   });
 });
