bed4ff35616f42fc6486fc4c5afe6b81280b6e82
Erlan-Bam <erlanzh.gg@gmail.com>
2024-05-29 13:43:17 +0500

    --CORS FIXED

--- server/dist/server.ts
+++ server/dist/server.ts
@@ -23,9 +23,6 @@ const app = express();
 export const buildRoute = path.join(__dirname, "templates/build/");
 export const uploadPath = path.join(__dirname, "uploads");
 
-app.use(bodyParser.json({ limit: '50mb' }));
-app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
-
 // Разрешены все Origins
 const corsOptions = {
   origin: function (origin, callback) {
@@ -37,6 +34,9 @@ const corsOptions = {
 };
 app.use(cors(corsOptions));
 app.options("*", cors(corsOptions));
+
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 
 // Создание сваггера
 app.use(
