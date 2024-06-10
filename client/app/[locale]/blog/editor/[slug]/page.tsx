2d451412367a42b9799abceb19547512caae6c95
senseiGai <swerss564@gmail.com>
2024-06-10 13:08:07 +0500

    NOT STYLED WYSWIG DONE

--- client/app/[locale]/blog/editor/[slug]/page.tsx
+++ client/app/[locale]/blog/editor/[slug]/page.tsx
@@ -1,7 +1,30 @@
-import React from "react";
+"use client";
 
-const BlogEditorPage = () => {
-  return <div>BlogEditorPage</div>;
+import React from "react";
+import ReactQuill from "react-quill";
+import EditorToolbar, { modules, formats } from "@features/QuillToolBar";
+import "react-quill/dist/quill.snow.css";
+import style from "./styles.module.scss";
+
+export const Editor = () => {
+  const [state, setState] = React.useState({ value: null });
+  const handleChange = (value) => {
+    setState({ value });
+  };
+  return (
+    <>
+      <EditorToolbar />
+      <ReactQuill
+        theme="snow"
+        value={state.value}
+        onChange={handleChange}
+        placeholder={"Write something awesome..."}
+        modules={modules}
+        formats={formats}
+        className={style.wyswig}
+      />
+    </>
+  );
 };
 
-export default BlogEditorPage;
+export default Editor;
