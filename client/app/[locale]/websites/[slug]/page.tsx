af72bfe2a31f4bfb6560f467b1899a852ad51708
k1ngsterr1 <ruslanmakhmatov@gmail.com>
2024-06-12 13:16:37 +0500

    --UPDATED STYLES

--- client/app/[locale]/websites/[slug]/page.tsx
+++ client/app/[locale]/websites/[slug]/page.tsx
@@ -11,7 +11,7 @@ export default function WebsitePage() {
   const { htmlContent } = useFetchUploadedPage(slug);
 
   return (
-    <div className="flex flex-col h-full">
+    <div className={`flex flex-col ${styles.page}`}>
       <HeaderEditor
         isLoading={false}
         websiteName="Test"
