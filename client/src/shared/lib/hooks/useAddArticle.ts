72e280ce2d64437759c9261a681f3629eea76f82
Ruslan Makhmatov <103198805+k1ngsterr1@users.noreply.github.com>
2024-06-28 01:28:20 +0600

    --NEW BUILD ADDED

--- client/src/shared/lib/hooks/useAddArticle.ts
+++ client/src/shared/lib/hooks/useAddArticle.ts
@@ -9,21 +9,21 @@ export function useAddArticle() {
   const addArticle = async (content: string, code: string) => {
     try {
       const formData = new FormData();
-      formData.append('content', content);
-      formData.append('code', code);
+      formData.append("content", content);
+      formData.append("code", code);
 
       const response = await axios.post(
         "https://ferla-backend-production.up.railway.app/api/articles/add",
         formData,
         {
           headers: {
-            'Content-Type': 'application/json',
-          }
+            "Content-Type": "application/json",
+          },
         }
       );
       console.log("Response data:", response.data);
       setArticleData(response.data);
-    //   window.location.reload(); 
+      //   window.location.reload();
     } catch (error) {
       console.error("There was an error with adding the article:", error);
     }
