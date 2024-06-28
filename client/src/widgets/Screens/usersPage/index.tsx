445824bf9f99f5ecd1853d33a0dc0138dae0850f
Ruslan Makhmatov <103198805+k1ngsterr1@users.noreply.github.com>
2024-06-28 12:00:19 +0600

    --ADDED TRANSLATION

--- client/src/widgets/Screens/usersPage/index.tsx
+++ client/src/widgets/Screens/usersPage/index.tsx
@@ -1,11 +1,12 @@
 import { Button } from "@shared/ui/Buttons_Components/Buttons";
-import styles from "./styles.module.scss";
-import Heading from "@shared/ui/Heading";
 import { UserTab } from "@entities/Tabs_Components/UserTab/index";
 import { useGetUsers } from "@shared/lib/hooks/Websites/useGetUsers";
-import SkeletonLoader from "@shared/ui/Skeleton_Loader";
-
 import { EmptySvg } from "@assets/index";
+import { useTranslations } from "next-intl";
+import SkeletonLoader from "@shared/ui/Skeleton_Loader";
+import Heading from "@shared/ui/Heading";
+
+import styles from "./styles.module.scss";
 
 interface UsersProps {
   users: [];
@@ -13,6 +14,7 @@ interface UsersProps {
 
 export const Users: React.FC<UsersProps> = ({ users }) => {
   const { isLoading, hasUsers } = useGetUsers();
+  const t = useTranslations("Users");
 
   if (isLoading) {
     return (
@@ -27,17 +29,15 @@ export const Users: React.FC<UsersProps> = ({ users }) => {
       <>
         <div className={styles.container}>
           <div className="flex w-[90%] justify-between items-center m-auto ">
-            <Heading text="Управление пользователями" />
+            <Heading text={t("heading")} />
             <Button
-              text="Добавить пользователя"
+              text={t("add_user")}
               buttonType="regular--small"
               functionType="userPopup"
             />
           </div>
           <EmptySvg className={styles.container__image} />
-          <p className={styles.container__already}>
-            У вас еще нет пользователей
-          </p>
+          <p className={styles.container__already}>{t("no_users")}</p>
         </div>
       </>
     );
@@ -45,9 +45,9 @@ export const Users: React.FC<UsersProps> = ({ users }) => {
   return (
     <div className={styles.users}>
       <div className="flex w-[90%] justify-between items-center m-auto ">
-        <Heading text="Управление пользователями" />
+        <Heading text={t("heading")} />
         <Button
-          text="Добавить пользователя"
+          text={t("add_user")}
           buttonType="regular--small"
           functionType="userPopup"
         />
